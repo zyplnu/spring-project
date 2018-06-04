4489b06686c9bd681b363030ee318edce1c51307
youngFF <597605057@qq.com>
2018-06-04 10:52:05 +0800

    弄清ClasspathXmlApplicationContext在哪里加载配置文件

--- spring-autowiredBean/src/test/java/com/hyena/spring/XmlConfigTest.java
+++ spring-autowiredBean/src/test/java/com/hyena/spring/XmlConfigTest.java
@@ -15,7 +15,10 @@ public class XmlConfigTest {
     @Test
     public void test() {
         ClassPathXmlApplicationContext context =
-                new ClassPathXmlApplicationContext("beans.xml");
+                // com/hyena/spring/xmlconfig/beans.xml是classpath
+                new ClassPathXmlApplicationContext("com/hyena/spring/xmlconfig/beans.xml");
+        // getBean方法首先查看Bean标签的name属性，如果没有则查看id属性----下面的首先查看name属性为user的，其次查看id属性为user的
+
         Object user = context.getBean("user");
         System.out.println(user instanceof User);
     }
